37a4980ac8c6ee37afb5d32487d5a829678668ef
CristinaMoraru <lilianacristina.moraru@uni-due.de>
2026-01-16 10:23:58 +0100

    v1.2.0

--- docs/versions.js
+++ docs/versions.js
@@ -1,7 +1,8 @@
 var DOC_VERSIONS = [
+    "v1.2.0",
     "v1.0.0",
     "v0.9.0",
     "v0.8.0",
   ];
-var DOCUMENTER_NEWEST = "v1.0.0";
-var DOCUMENTER_STABLE = "v1.0.0";
+var DOCUMENTER_NEWEST = "v1.2.0";
+var DOCUMENTER_STABLE = "v1.2.0";
